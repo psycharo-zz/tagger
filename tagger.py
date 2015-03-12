b368c1c7a1f67352e2c9196851e6745d9659c177
Timur <psycharo@gmail.com>
2015-03-12 17:58:53 +0100

    Update tagger.py

--- tagger.py
+++ tagger.py
@@ -18,17 +18,6 @@ GREEN_COLOR = [0,1,0]
 RED_COLOR = [0,0,1]
 YELLOW_COLOR = [0,1,1]
 
-# HOWTO
-# when you switch frames, the current tag points are saved automatically and copied to the next frame
-# ']' - next frame
-# '[' - prev frame
-# 'q' - quit
-# 'w' - write the ground truth to the file
-# 's' - save currently added points to the target
-# 'd' - delete point added at this run
-# 'p' - completely delete all the points for the current target
-# '0'-'9' - select current target number
-
 def main():
     curr_points = []
 
