56d97dcb7cc2c3758c61c13fc4206cc3fb64fd80
Timur Bagautdinov <timur.bagautdinov@epfl.ch>
2015-04-16 15:48:06 +0200

    added scaling and matlab output

--- tagger.py
+++ tagger.py
@@ -2,9 +2,10 @@
 
 import cv2
 import numpy as np
+from scipy.io import savemat
 import os, sys, time, glob, copy, pickle
 
-WINDOW_NAME = 'current frame'
+WINDOW_NAME = 'tagger'
 
 KEY_SPACE = 32
 KEY_DEL = 127
@@ -13,10 +14,10 @@ KEY_DOWN = 63233
 KEY_LEFT = 63234
 KEY_RIGHT = 63235
 
-BLUE_COLOR = [1,0,0]
-GREEN_COLOR = [0,1,0]
-RED_COLOR = [0,0,1]
-YELLOW_COLOR = [0,1,1]
+BLUE_COLOR = [255,0,0]
+GREEN_COLOR = [0,255,0]
+RED_COLOR = [0,0,255]
+YELLOW_COLOR = [0,255,255]
 
 def main():
     curr_points = []
@@ -27,7 +28,18 @@ def main():
 
     def draw_selected_points(pts, color):
         for p in pts:
-            cv2.circle(frame, tuple(p), 2, color, 1)
+            cv2.circle(frame, p, 2, color, 1)
+
+    def rescale_points(pts, f):
+        return [(int(p[0] * f), int(p[1] * f)) for p in pts]
+
+    def rescale_gt(gt, f):
+        gt_rescaled = {}
+        for fid in gt:
+            gt_rescaled[fid] = {}
+            for oid in gt[fid]:
+                gt_rescaled[fid][oid] = {'points' : rescale_points(gt[fid][oid]['points'], f)}
+        return gt_rescaled
 
     def update_gt(overwrite=False):
         if len(curr_points) == 0:
@@ -37,7 +49,21 @@ def main():
             gt[fid] = {}
 
         if not curr_oid in gt[fid] or overwrite:
-            gt[fid][curr_oid] = {'points_image': copy.deepcopy(curr_points)}
+            gt[fid][curr_oid] = {'points': copy.deepcopy(curr_points)}
+
+    def save_gt(gt):
+        # taking into account image resize
+        gt_rescaled = rescale_gt(gt, 1.0 / resize_factor)
+        pickle.dump(gt_rescaled, open(os.path.join(gt_path, 'ground_truth.pickle'), 'w'))
+
+        # matlab
+        gt_mat = {}
+        for fid in gt_rescaled:
+            gt_mat['f%d' % fid] = {} 
+            for oid in gt_rescaled[fid]:
+                gt_mat['f%d' % fid]['o%d' % oid] = gt_rescaled[fid][oid]
+        savemat(os.path.join(gt_path, 'ground_truth.mat'), gt_mat)    
+        
 
     cv2.namedWindow(WINDOW_NAME)
     cv2.setMouseCallback(WINDOW_NAME, on_mouse_event)
@@ -45,8 +71,8 @@ def main():
     fid = 0
 
     gt = {fid: {}}
-    if os.path.exists(gt_path + 'ground_truth.pickle'):
-        gt = pickle.load(open(gt_path + 'ground_truth.pickle'))
+    if os.path.exists(os.path.join(gt_path, 'ground_truth.pickle')):
+        gt = rescale_gt(pickle.load(open(os.path.join(gt_path, 'ground_truth.pickle'))), resize_factor)
 
     curr_oid = 0
     frame = frames[fid]
@@ -57,11 +83,12 @@ def main():
 
         frame = np.copy(frames[fid])
         cv2.putText(frame, str(fid), (20,20), cv2.FONT_HERSHEY_PLAIN, 1, YELLOW_COLOR, 1)
+        cv2.putText(frame, 'class: %d' % curr_oid, (frame.shape[1]-80,20), cv2.FONT_HERSHEY_PLAIN, 1, YELLOW_COLOR, 1)        
 
         draw_selected_points(curr_points, RED_COLOR)
 
         if fid in gt and curr_oid in gt[fid]:
-            pts = gt[fid][curr_oid]['points_image']
+            pts = gt[fid][curr_oid]['points']
             draw_selected_points(pts, BLUE_COLOR)
             # drawing text
             p = tuple(np.mean(np.vstack(pts), 0).astype(np.int))
@@ -75,6 +102,10 @@ def main():
             fid -= 1
         elif key == ord(']') and not fid == end_fid:
             fid += 1
+        elif key == ord('='):
+            curr_oid += 1
+        elif key == ord('-'):
+            curr_oid = max(0, curr_oid-1)
         elif key == KEY_DEL:
             if fid in gt and curr_oid in gt[fid]:
                 del gt[fid][curr_oid]
@@ -87,8 +118,9 @@ def main():
         elif key == ord('d'):
             curr_points = []
         elif key == ord('w'):
-            pickle.dump(gt, open(gt_path + 'ground_truth.pickle', 'w'))
-            pickle.dump(gt, open(tmp_path + 'ground_truth.%d.pickle' % int(time.time()), 'w'))
+            save_gt(gt)
+            pickle.dump(gt, open(os.path.join(tmp_path, 'ground_truth.%d.pickle' % int(time.time())), 'w'))
+            
         elif 0 <= oid <= 9:
             curr_oid = oid
         elif key == ord('q'):
@@ -98,17 +130,18 @@ def main():
             update_gt()
             frame = frames[fid]
 
-    pickle.dump(gt, open(gt_path + 'ground_truth.pickle', 'w'))
+    save_gt(gt)
     cv2.destroyAllWindows()
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print 'Usage:'
-    print 'tagger.py <dataset_path> <format, e.g. rgb%06d.png>'
+    print 'tagger.py <dataset_path> <format, e.g. rgb%06d.png> <resize factor>'
     exit(-1)
 
 # you can also just fill these in
 dataset_path = os.path.abspath(sys.argv[1]) #'/Users/timur/Documents/data/kinect2/ours-drones/seq3/'
 input_format = os.path.join(dataset_path, sys.argv[2]) #'depth%06d.png'
+resize_factor = float(sys.argv[3])
 # where to save/look for the ground truth
 gt_path = dataset_path
 # where to save temporary ground truth
@@ -123,6 +156,6 @@ if not os.path.exists(gt_path):
 
 print 'loading data from %s, frames %d to %d' % (input_format, start_fid, end_fid)
 
-frames = { fid : cv2.imread(input_format % fid).astype(np.double) / 255.0
+frames = { fid : cv2.resize(cv2.imread(input_format % fid), (0,0), None, resize_factor, resize_factor)
            for fid in range(start_fid,end_fid+1) }
 main()
